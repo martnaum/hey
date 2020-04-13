117feead819bf90da1007945f20eddf99f6f759c
Martin Naumann <19922931+martnaum@users.noreply.github.com>
2020-04-13 15:13:04 +0200

    add social icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { SocialIcon } from 'react-social-icons';
 
 function App() {
   return (
@@ -7,6 +8,10 @@ function App() {
       <header className="App-header">
         <h1>martnaum</h1>
       </header>
+      <section className="social">
+        <SocialIcon className="social-icon" url="https://linkedin.com/in/martin-naumann"/>
+        <SocialIcon className="social-icon" url="https://github.com/martnaum"/>
+      </section>
     </div>
   );
 }
