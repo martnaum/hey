9dd4db03b4fffde9eb00cc7e7a476005648d2acb
Martin Naumann <19922931+martnaum@users.noreply.github.com>
2020-06-21 10:35:38 +0200

    fix flex layout

--- src/App.js
+++ src/App.js
@@ -6,37 +6,39 @@ import me from './assets/me.png';
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Hey, I am Martin!</h1>
-        <section>
-          <img className="image" src={me} alt="me"/>
-          <p>Beat ordinary thinking.</p>
+      <section className="base">
+        <header>
+          <h1>Hey, I am Martin!</h1>
+          <section>
+            <img className="image" src={me} alt="me"/>
+            <p>Beat ordinary thinking.</p>
+          </section>
+        </header>
+        <section className="description">
+          <section className="descriptionBlock">
+            <p>Agile Project Manager</p>
+            <p>Software Developer</p>
+          </section>
+          <section className="descriptionBlock">
+            <p>Team Leader and Mentor</p>
+            <p>Troubleshooter</p>
+          </section>
         </section>
-      </header>
-      <section className="description">
-        <section className="descriptionBlock">
-          <p>Agile Project Manager</p>
-          <p>Software Developer</p>
-        </section>
-        <section className="descriptionBlock">
-          <p>Team Leader and Mentor</p>
-          <p>Troubleshooter</p>
-        </section>
+        <footer>
+          <SocialIcon className="social-icon" fgColor="grey" bgColor="transparent"
+            url="https://linkedin.com/in/martnaum" target="_blank" rel="noopener noreferrer"
+          />
+          <SocialIcon className="social-icon" fgColor="grey" bgColor="transparent"
+            url="https://github.com/martnaum" target="_blank" rel="noopener noreferrer"
+          />
+          <SocialIcon className="social-icon" fgColor="grey" bgColor="transparent"
+            url="https://twitter.com/martnaum" target="_blank" rel="noopener noreferrer"
+          />
+          <SocialIcon className="social-icon" fgColor="grey" bgColor="transparent"
+            url="mailto:martin@naumann.world"
+          />
+        </footer>
       </section>
-      <footer>
-        <SocialIcon className="social-icon" fgColor="grey" bgColor="transparent"
-          url="https://linkedin.com/in/martnaum" target="_blank" rel="noopener noreferrer"
-        />
-        <SocialIcon className="social-icon" fgColor="grey" bgColor="transparent"
-          url="https://github.com/martnaum" target="_blank" rel="noopener noreferrer"
-        />
-        <SocialIcon className="social-icon" fgColor="grey" bgColor="transparent"
-          url="https://twitter.com/martnaum" target="_blank" rel="noopener noreferrer"
-        />
-        <SocialIcon className="social-icon" fgColor="grey" bgColor="transparent"
-          url="mailto:martin@naumann.world"
-        />
-      </footer>
     </div>
   );
 }
