607c5da0eb9ec68880315cd85c3511cb7d6b012b
Martin Naumann <19922931+martnaum@users.noreply.github.com>
2020-12-23 11:10:24 +0100

    Updates

---
+++ precache-manifest.3733ba769b1fe0179af6faa04fa8d523.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a535c7deeaea3ce4b38a79649c7544",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b37d65782d02ceee8910",
+    "url": "/static/css/main.f3a76829.chunk.css"
+  },
+  {
+    "revision": "8e5927b1c3859c0441b3",
+    "url": "/static/js/2.3237ec53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3237ec53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37d65782d02ceee8910",
+    "url": "/static/js/main.b77aa71d.chunk.js"
+  },
+  {
+    "revision": "ca37e04686475f066e3b",
+    "url": "/static/js/runtime-main.448a3e6c.js"
+  },
+  {
+    "revision": "900f2c4e87851d23f721e4f20dc0d088",
+    "url": "/static/media/me.900f2c4e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64de2dfbd95ad819e20c1eea22c87627.js"
+  "/precache-manifest.3733ba769b1fe0179af6faa04fa8d523.js"
 );
 
 self.addEventListener('message', (event) => {
